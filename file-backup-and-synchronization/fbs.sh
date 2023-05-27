41a8d75ba2976e24c5d020f8cfde7de435d602f5
Alia Mahama-Rodríguez <62684338+aliamrod@users.noreply.github.com>
2023-05-27 12:40:52 -0500

    Update fbs.sh

--- file-backup-and-synchronization/fbs.sh
+++ file-backup-and-synchronization/fbs.sh
@@ -1,1 +1,12 @@
-**
+# FILE BACKUP AND SYNCHRONIZATION
+# Script constructed to automate the backup and synchronization of files and directories to a remote location
+# or cloud storage, with options for scheduling, incremenetal backups, and encryption.
+
+#! /bin/bash
+
+# Configuration variables
+SOURCE_DIR="/Users/aliamahama-rodriguez/source_dir" # /path/to/source_directory
+DEST_DIR="/Users/aliamahama-rodriguez/destination_dir" # /path/to/destination_directory
+SCHEDULE="0 1 * * " # Cron schedule expression (every day at 1 AM)
+INCREMENETAL_BACKUP=false
+ENCRYPTION=false
